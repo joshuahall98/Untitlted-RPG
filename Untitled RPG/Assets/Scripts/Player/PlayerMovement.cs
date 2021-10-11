61a0d29070d068c932a94e670b22c43377b751ea
Jake Horner <jakehorner19@gmail.com>
2021-10-11 20:50:37 +0100

    folder

---
+++ Untitled RPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInputActions controller;
+    private Rigidbody2D rb; 
+
+    [SerializeField] public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controller = new PlayerInputActions();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable(){
+        controller.Enable();
+    }
+
+    private void OnDisable(){
+        controller.Disable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 moveInput = controller.Movement.Move.ReadValue<Vector2>();
+        rb.velocity = moveInput * speed;
+    }
+}
