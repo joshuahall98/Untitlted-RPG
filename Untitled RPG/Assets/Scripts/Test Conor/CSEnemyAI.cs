8aab3d56eaf5e4e9062f7ef849e67cb0598114c3
Jake Horner <jakehorner19@gmail.com>
2021-10-13 23:57:00 +0100

    anim start

--- Untitled RPG/Assets/Scripts/Test Conor/CSEnemyAI.cs
+++ Untitled RPG/Assets/Scripts/Test Conor/CSEnemyAI.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 
 public class CSEnemyAI : MonoBehaviour
 {
+
+    public Animator anim;
     public float speed;
 
     private Transform target;
 
+    public enum State { aggressive, passive }
+    public State action = State.passive;
+
     private void Start()
     {
+        //get Animator component from object
+        anim.GetComponent<Animator>();
+        //sets state to aggressive
+        action = State.aggressive;
+        //sets state to passive
+        //action = State.passive;
+        //sets anim to desired action
+        anim.SetInteger("State", (int)action);
+
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        
     }
     private void Update()
     {
-       
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         
     }
 }
