5f18447687c37dda1be544f50a9146a3f429b428
NurehaFox <conorsheridan@live.ie>
2021-10-14 15:40:20 +0100

    BasicPatrol

--- Untitled RPG/Assets/Scripts/Test Conor/CSEnemyAI.cs
+++ Untitled RPG/Assets/Scripts/Test Conor/CSEnemyAI.cs
@@ -7,7 +7,21 @@ public class CSEnemyAI : MonoBehaviour
 {
 
     public Animator anim;
+    public float distStop;
     public float speed;
+
+    //Line of Sight Variable
+    public float lineOfSightDist;
+
+    //Randomise Patrol Variables
+
+    public float startWaitTime;
+    private float waitTime;
+    public Transform moveSpot;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
 
     private Transform target;
 
@@ -27,14 +41,45 @@ public class CSEnemyAI : MonoBehaviour
         //sets anim to desired action
         anim.SetInteger("State", (int)action);
 
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(UnityEngine.Random.Range(minX, maxX), UnityEngine.Random.Range(minY, maxY));
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 
-        
+
     }
     private void Update()
     {
 
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        
+        //Line of Sight
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, moveSpot.position, lineOfSightDist);
+        if (hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + moveSpot.position * lineOfSightDist, Color.green);
+        }
+
+        //Follow Player endlessly
+        // transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot.position = new Vector2(UnityEngine.Random.Range(minX, maxX), UnityEngine.Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
+        }
+
     }
 }
+
